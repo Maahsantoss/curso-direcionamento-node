1e9fdd2db424eec8f044d773683747cefe552122
unknown <m_camila_santos@hotmail.com>
2022-10-20 18:52:45 -0300

    Subindo arquivos de comparaões, operador tenario e template-strings

---
+++ template-strings.js
@@ -0,0 +1,18 @@
+// const nome = "Maria Camila";
+// const idade = 2022-1993;
+// const cidadeDeNascimento = "Ribeirão Preto";
+
+//const apresentacao = "Meu nome é: " + nome + ", " + "minha idade é "+ idade + " e nasci em: " + cidadeDeNascimento;
+
+// const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} anos e nasci na cidade de ${cidadeDeNascimento}`;
+
+// console.log(apresentacao);
+
+const cliente = "Luciano";
+const idade = 23;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
+const pedido = `Meu nome é ${cliente} e eu vou querer ${idade >= 18 ? bebidaMaior : bebidaMenor}`;
+
+console.log(pedido)
---
+++ tenario.js
@@ -0,0 +1,11 @@
+
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+// if(idadeCliente >= idadeMinima){
+//     console.log("O cliente pode beber");
+// } else {
+//     console.log("O cliente não pode beber");
+// }
+
+console.log(idadeCliente >= idadeMinima ? "O Cliente pode beber" : "O Cliente não pode beber");
