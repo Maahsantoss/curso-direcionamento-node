a21b460a9e11a5fb33e52d6048d91cb30ba9c9db
unknown <m_camila_santos@hotmail.com>
2022-10-20 16:29:54 -0300

    Adicionando arquivos de conversoes, erros e console

--- conversoes.js
+++ conversoes.js
@@ -7,3 +7,5 @@ const numeroString = "456";
 console.log(numero + Number(numeroString));
 
 //conversao explicita
+let telefone = 981109449;
+console.log("O numero do telefone é" + telefone.toString());
